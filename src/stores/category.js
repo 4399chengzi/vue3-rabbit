8b4f92a63fb262accfc9ced6eb692292e9f765e7
4399chengzi <981691590@qq.com>
2023-06-08 10:40:32 +0800

    优化导航数据

---
+++ src/stores/category.js
@@ -0,0 +1,19 @@
+import { ref } from 'vue'
+import { defineStore } from 'pinia'
+import { getCategoryAPI } from '@/apis/layout'
+
+
+export const useCategoryStore = defineStore('category', () => {
+    const categoryList = ref([])
+
+    const getCategory = async () => {
+      const res = await getCategoryAPI()
+      console.log(res);
+      categoryList.value = res.result
+    }
+
+    return {
+        categoryList,
+        getCategory
+    }
+})
--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -3,7 +3,10 @@ import LayoutNav from './components/LayoutNav.vue'
 import LayoutHeader from './components/LayoutHeader.vue'
 import LayoutFooter from './components/LayoutFooter.vue'
 import LayoutFixed from './components/LayoutFixed.vue';
-
+import { useCategoryStore } from '@/stores/category';
+import { onMounted } from 'vue';
+const categoryStores = useCategoryStore()
+onMounted(() => categoryStores.getCategory())
 </script>
 
 <template>
