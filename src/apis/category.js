7c6fa1676c8affc75f317c9ef79e00d47920b42e
4399chengzi <981691590@qq.com>
2023-06-09 15:59:39 +0800

    一级分类-面包屑导航渲染

---
+++ src/apis/category.js
@@ -0,0 +1,16 @@
+import request from '@/utils/http'
+
+export function getCategoryAPI (id){
+    return request({
+        url: '/category',
+        params: {
+            id
+        }
+    })
+}
+
+
+
+
+
+
