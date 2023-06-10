4f010415e5cd816253c99aab82e82807f82aa56f
4399chengzi <981691590@qq.com>
2023-06-10 19:57:04 +0800

    详情热榜-title和列表内容的适配

--- src/views/Detail/components/DetailHot.vue
+++ src/views/Detail/components/DetailHot.vue
@@ -1,14 +1,28 @@
 <script setup>
 import { getHotGoodsAPI } from '@/apis/detail';
+import { computed } from 'vue';
 import { onMounted, ref } from 'vue';
 import { useRoute } from 'vue-router';
+
+const props = defineProps({
+    hotType: {
+        type: Number
+    }
+})
+
+const TYPEMAP = {
+    1: "24小时热榜",
+    2: "周热榜"
+}
+
+const title = computed(() => TYPEMAP[props.hotType])
 
 const hotList = ref([])
 const route = useRoute()
 const getHotList = async () => {
     const res = await getHotGoodsAPI({
         id: route.params.id,
-        type: 1
+        type: props.hotType
     })
     hotList.value = res.result
 }
@@ -19,60 +33,60 @@ onMounted(() => getHotList())
 
 
 <template>
-  <div class="goods-hot">
-    <h3>周日榜单</h3>
-    <!-- 商品区块 -->
-    <RouterLink to="/" class="goods-item" v-for="item in hotList" :key="item.id">
-      <img :src="item.picture" alt="" />
-      <p class="name ellipsis">{{ item.name }}</p>
-      <p class="desc ellipsis">{{ item.desc }}</p>
-      <p class="price">&yen;{{ item.price }}</p>
-    </RouterLink>
-  </div>
+    <div class="goods-hot">
+        <h3>{{ title }}</h3>
+        <!-- 商品区块 -->
+        <RouterLink to="/" class="goods-item" v-for="item in hotList" :key="item.id">
+            <img :src="item.picture" alt="" />
+            <p class="name ellipsis">{{ item.name }}</p>
+            <p class="desc ellipsis">{{ item.desc }}</p>
+            <p class="price">&yen;{{ item.price }}</p>
+        </RouterLink>
+    </div>
 </template>
 
 
 <style scoped lang="scss">
 .goods-hot {
-  h3 {
-    height: 70px;
-    background: $helpColor;
-    color: #fff;
-    font-size: 18px;
-    line-height: 70px;
-    padding-left: 25px;
-    margin-bottom: 10px;
-    font-weight: normal;
-  }
-
-  .goods-item {
-    display: block;
-    padding: 20px 30px;
-    text-align: center;
-    background: #fff;
-
-    img {
-      width: 160px;
-      height: 160px;
+    h3 {
+        height: 70px;
+        background: $helpColor;
+        color: #fff;
+        font-size: 18px;
+        line-height: 70px;
+        padding-left: 25px;
+        margin-bottom: 10px;
+        font-weight: normal;
     }
 
-    p {
-      padding-top: 10px;
-    }
+    .goods-item {
+        display: block;
+        padding: 20px 30px;
+        text-align: center;
+        background: #fff;
 
-    .name {
-      font-size: 16px;
-    }
+        img {
+            width: 160px;
+            height: 160px;
+        }
 
-    .desc {
-      color: #999;
-      height: 29px;
-    }
+        p {
+            padding-top: 10px;
+        }
 
-    .price {
-      color: $priceColor;
-      font-size: 20px;
+        .name {
+            font-size: 16px;
+        }
+
+        .desc {
+            color: #999;
+            height: 29px;
+        }
+
+        .price {
+            color: $priceColor;
+            font-size: 20px;
+        }
     }
-  }
 }
 </style>
