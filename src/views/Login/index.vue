cc5d8b615069e9918ba2df6009fdd95629cf4768
4399chengzi <981691590@qq.com>
2023-06-12 13:21:19 +0800

    登录-表单校验-统一校验

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -30,6 +30,16 @@ const rules = ref({
     ]
 })
 
+const formRef = ref(null)
+const doLogin = () => {
+    formRef.value.validate((valid) => {
+        console.log(valid)
+        if(valid){
+            // TODO LOGIN
+        }
+    })
+}
+
 </script>
 
 
@@ -54,7 +64,7 @@ const rules = ref({
                 </nav>
                 <div class="account-box">
                     <div class="form">
-                        <el-form :model="form" :rules="rules" label-position="right" label-width="60px" status-icon>
+                        <el-form ref="formRef" :model="form" :rules="rules" label-position="right" label-width="60px" status-icon>
                             <el-form-item prop="account" label="账户">
                                 <el-input v-model="form.account"/>
                             </el-form-item>
@@ -66,7 +76,7 @@ const rules = ref({
                                     我已同意隐私条款和服务条款
                                 </el-checkbox>
                             </el-form-item>
-                            <el-button size="large" class="subBtn">点击登录</el-button>
+                            <el-button size="large" class="subBtn" @click="doLogin">点击登录</el-button>
                         </el-form>
                     </div>
                 </div>
