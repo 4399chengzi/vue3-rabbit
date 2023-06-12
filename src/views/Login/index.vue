e79c583a16f991ea509c89544c80fe7aa540b9d5
4399chengzi <981691590@qq.com>
2023-06-12 12:58:46 +0800

    登录-表单校验-自定义校验

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -4,7 +4,8 @@ import { ref } from 'vue';
 
 const form = ref({
     account: '',
-    password: ''
+    password: '',
+    agree: true
 })
 
 const rules = ref({
@@ -14,6 +15,18 @@ const rules = ref({
     password: [
         { required: true, message: '密码不能为空', trigger: 'blur' },
         { min: 6, max: 14, message: '密码长度为6-14个字符', trigger: 'blur' }
+    ],
+    agree: [
+        {
+            validator: (rule, value, callback) => {
+                console.log(value)
+                if(value){
+                    callback()
+                }else{
+                    callback(new Error('请勾选协议'))
+                }
+            }
+        }
     ]
 })
 
@@ -48,8 +61,8 @@ const rules = ref({
                             <el-form-item prop="password" label="密码">
                                 <el-input v-model="form.password" />
                             </el-form-item>
-                            <el-form-item label-width="22px">
-                                <el-checkbox size="large">
+                            <el-form-item prop="agree" label-width="22px">
+                                <el-checkbox size="large" v-model="form.agree">
                                     我已同意隐私条款和服务条款
                                 </el-checkbox>
                             </el-form-item>
