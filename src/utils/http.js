48c942e1cb858bf9c8ad3a1e37052d797f30aebd
4399chengzi <981691590@qq.com>
2023-06-14 15:57:21 +0800

    登录-token拦截器中携带

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 import { ElMessage } from 'element-plus'
-import 'element-plus/es/components/message/style/css'
+import { useUserStore } from "@/stores/user"
 
 
 const httpInstance = axios.create({
