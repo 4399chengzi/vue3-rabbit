a8bbced27a7cf45ee01c8a94d2ab95a089c29fbe
4399chengzi <981691590@qq.com>
2023-07-14 08:24:16 +0800

    购物车-统计数据

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -18,23 +18,39 @@ export const useCartStore = defineStore('cart', () => {
         cartList.value.splice(idx, 1)
     }
 
-    const singleCheck = (skuId, selected) =>{
-        const item = cartList.value.find((item)=>item.skuId === skuId)
+    const singleCheck = (skuId, selected) => {
+        const item = cartList.value.find((item) => item.skuId === skuId)
         item.selected = selected
+    }
+
+    // 全选功能
+    const allCheck = (selected) => {
+        // 把cartList中的每一项的selected都设置为当前的全选框状态
+        cartList.value.forEach((item) => item.selected = selected)
     }
 
     const allCount = computed(() => cartList.value.reduce((a, c) => a + c.count, 0))
     const allPrice = computed(() => cartList.value.reduce((a, c) => a + c.count * c.price, 0))
 
+    const selectedCount = computed(() => cartList.value.filter(item => item.selected).reduce((a, c) => a + c.count, 0))
+
+    const selectedPrice = computed(() => cartList.value.filter(item => item.selected).reduce((a, c) => a + c.count * c.price, 0))
+
+
+    const isAll = computed(() => cartList.value.every((item) => item.selected))
 
 
     return {
         cartList,
         allCount,
         allPrice,
+        isAll,
+        selectedCount,
+        selectedPrice,
         addCart,
         delCart,
-        singleCheck
+        singleCheck,
+        allCheck
     }
 }, {
     persist: true,
--- src/views/CartList/index.vue
+++ src/views/CartList/index.vue
@@ -6,6 +6,10 @@ const singleCheck = (i, selected) => {
     console.log(i, selected)
     cartStore.singleCheck(i.skuId, selected)
 }
+
+const allCheck = (selected) => {
+  cartStore.allCheck(selected)
+}
 </script>
 
 <template>
@@ -16,7 +20,7 @@ const singleCheck = (i, selected) => {
                     <thead>
                         <tr>
                             <th width="120">
-                                <el-checkbox />
+                                <el-checkbox :model-value="cartStore.isAll" @change="allCheck"/>
                             </th>
                             <th width="400">商品信息</th>
                             <th width="220">单价</th>
@@ -77,8 +81,8 @@ const singleCheck = (i, selected) => {
             <!-- 操作栏 -->
             <div class="action">
                 <div class="batch">
-                    共 10 件商品，已选择 2 件，商品合计：
-                    <span class="red">¥ 200.00 </span>
+                    共 {{ cartStore.allCount }} 件商品，已选择 {{ cartStore.selectedCount }} 件，商品合计：
+                    <span class="red">¥ {{ cartStore.selectedPrice.toFixed(2) }} </span>
                 </div>
                 <div class="total">
                     <el-button size="large" type="primary">下单结算</el-button>
