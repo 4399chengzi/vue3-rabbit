d47411a3ba6c360ca042910d07484343519b3c6c
4399chengzi <981691590@qq.com>
2023-07-20 23:13:45 +0800

    结算-生成订单功能实现

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -4,7 +4,6 @@ import HomeBanner from './components/HomeBanner.vue'
 import HomeNew from './components/HomeNew.vue'
 import HomeHot from './components/HomeHot.vue'
 import homeProduct from './components/HomeProduct.vue'
-import HomePanel from './components/HomePanel.vue'
 </script>
 
 <template>
